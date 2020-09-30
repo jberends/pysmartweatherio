1788fccabe27cc8f7aeba9c1e13405cfe67f9fea
Bjarne Riis <bjarne@briis.com>
2020-09-30 10:05:40 +0200

    Bumped to 2.0.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'pysmartweatherio',
   packages = ['pysmartweatherio'],
-  version = '2.0.8',
+  version = '2.0.9',
   license='MIT',
   description = 'Python Wrapper for Smart Home Weather REST API', 
   long_description=" ".join(
